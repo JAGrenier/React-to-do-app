8d55fe1c66ddba32ca3dabcb38a9efdd8cdab80f
github username <github email address>
2020-10-09 11:42:59 -0600

    set up    todolist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoList from './Components/TodoList';
 
 class App extends Component {
+
+  state={
+    todos: [
+      {id: 1, title: "First To Do", content: "I have something to do!"},
+      {id: 2, title: "Second To Do", content: "I have something more to do!"},
+      {id: 3, title: "Third  To Do", content: "I have too much to do!"}
+    ]
+  }
+
   render(){
     return (
         <div>
-        
+          <TodoList />
         </div>
       );
   }
---
+++ src/Components/TodoList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoList(props) {
+    return (
+        <section className="to-do-list">
+            <h1>ToDoList Component</h1>
+        </section>
+    )
+}
